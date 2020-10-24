f90f21d45c17fc235b1909ce065dbfbdc8334489
Utkarsh <id.simpu@gmail.com>
2020-10-24 17:24:57 +0530

    Student form

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms import PasswordField, SubmitField, IntegerField, StringField, DateField
+from wtforms.validators import DataRequired, EqualTo, Email
 
 
 class LoginForm(FlaskForm):
@@ -14,3 +14,13 @@ class PasswordForm(FlaskForm):
     new_password = PasswordField('New Password', validators=[DataRequired()])
     repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('new_password')])
     submit = SubmitField('Change Password')
+
+
+class StudentForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    dob = DateField('Date of Birth', validators=[DataRequired()])
+    branch = StringField('Branch', validators=[DataRequired()])
+    minor = StringField('Minor', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
