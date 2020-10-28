dead0153a45a7465cf472cad6b4acf5eae5e4f0c
zeref14 <riteshsingh1498@gmail.com>
2020-10-28 18:38:26 +0530

    Added Resume basic template

--- app/generator/generator.py
+++ app/generator/generator.py
@@ -1,5 +1,7 @@
 from jinja2 import FileSystemLoader, Environment
 import os
+from flask_login import current_user
+from models import *
 
 latex_jinja_env = Environment(
     block_start_string=r'\BLOCK{',
@@ -17,5 +19,14 @@ latex_jinja_env = Environment(
 
 
 def render_latex(uid: int):
-    template = latex_jinja_env.get_template('generator/template.xtx')
-    return template.render(id=uid)
+    student=Student.load(current_user.id)
+    tenth=Tenth.load(current_user.id)
+    twelfth=Twelfth.load(current_user.id)
+    sgpa=SGPA.load(current_user.id)
+    skill=Skill.load(current_user.id)
+    language=Language.load(current_user.id)
+    achievement=Achievement.load(current_user.id)
+    extra_curr=ExtraCurricular.load(current_user.id)
+    intern=Internship.load(current_user.id)
+    template = latex_jinja_env.get_template('generator/CV.tex')
+    return template.render(intern=intern.intern_list,extra_curr=extra_curr.ec_list,achievement=achievement.achievement_list,language=language.language_list,student=student,tenth=tenth,twelfth=twelfth,sgpa=sgpa.sg_list,skill=skill.skill_list, id=uid)
